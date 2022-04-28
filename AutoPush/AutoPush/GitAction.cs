d4a56b24ddd52ff3902611dc9320247f41c104e7
rakib <rakib@email.com>
2022-04-28 14:18:43 +0600

    init

---
+++ AutoPush/AutoPush/GitAction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AutoPush
+{
+    public static class GitAction
+    {
+        static string gitBranch = @"master";
+        static string gitCommand = "git";
+        static string gitAddArgument = @"add -A";
+        static string gitCommitArgument = @"commit ""explanations_of_changes""";
+        static string gitPushArgument = @$"push {gitBranch}";
+        static string gitPullArgument = @$"pull {gitBranch}";
+
+        public static bool AutoPush()
+        {
+            try
+            {
+                _add();
+                _commit();
+                _push();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+           
+        }
+
+        private static void _add()
+        {
+            Process.Start(gitCommand, gitAddArgument);
+        }
+        private static void _commit()
+        {
+            Process.Start(gitCommand, gitCommitArgument);
+        }
+        private static void _push()
+        {
+            Process.Start(gitCommand, gitPushArgument);
+        }
+
+        private static void _pull()
+        {
+            Process.Start(gitCommand, gitPullArgument);
+        }
+
+    }
+}
